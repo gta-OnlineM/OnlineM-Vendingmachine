2bd4193efcb5e7ae2680b1a363024af4cf1e3e27
Kasey Fitton <kaseyfitton@hotmail.co.uk>
2024-01-28 17:20:56 +0000

    refactor(client/main): rewrite the resource
    massively improves performance

--- client/main.lua
+++ client/main.lua
@@ -19,14 +19,175 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.]]
 
-function Interact(Model, Object)
+VendingHandler = setmetatable({}, VendingHandler)
+VendingHandler.__index = VendingHandler
+VendingHandler.storage = {}
+VendingHandler.currentMachine = nil
+VendingHandler.currentIndex = nil
+VendingHandler.helpText = { showing = false, text = "" }
+
+------------- Performance Optimisation ------------------- 
+local CreateThread = CreateThread
+local Wait = Wait
+local GetEntityCoords = GetEntityCoords
+local GetClosestObjectOfType = GetClosestObjectOfType
+local DoesEntityExist = DoesEntityExist
+local joaat = joaat
+local exports = exports
+local IsNuiFocused = IsNuiFocused
+local IsControlEnabled = IsControlEnabled
+local IsEntityAtCoord = IsEntityAtCoord
+local TaskGoStraightToCoord = TaskGoStraightToCoord
+local TaskTurnPedToFaceEntity = TaskTurnPedToFaceEntity
+local RequestAnimDict = RequestAnimDict
+local HasAnimDictLoaded = HasAnimDictLoaded
+local RequestAmbientAudioBank = RequestAmbientAudioBank
+local HintAmbientAudioBank = HintAmbientAudioBank
+local SetPedCurrentWeaponVisible = SetPedCurrentWeaponVisible
+local RequestModel = RequestModel
+local HasModelLoaded = HasModelLoaded
+local SetPedResetFlag = SetPedResetFlag
+local GetPedBoneIndex = GetPedBoneIndex
+local AttachEntityToEntity = AttachEntityToEntity
+local SetEntityAsMissionEntity = SetEntityAsMissionEntity
+local SetEntityProofs = SetEntityProofs
+local ApplyForceToEntity = ApplyForceToEntity
+local SetEntityAsNoLongerNeeded = SetEntityAsNoLongerNeeded
+local ClearPedTasks = ClearPedTasks
+local ReleaseAmbientAudioBank = ReleaseAmbientAudioBank
+local RemoveAnimDict = RemoveAnimDict
+local SetModelAsNoLongerNeeded = SetModelAsNoLongerNeeded
+local TriggerEvent = TriggerEvent
+local TriggerServerEvent = TriggerServerEvent
+-----------------------------------------------------------------
+
+function VendingHandler:RegisterMachine(model, distance, helpText)
+    self.storage[#self.storage + 1] = {
+        model = model,
+        distance = distance,
+        helpText = helpText,
+    }
+end
+
+function VendingHandler:RegisterTargets()
+    for i = 1, #(Config.Models) do
+        local options = {
+            {
+                name = Config.Models[i].model .. '-vend',
+                icon = 'fa-solid fa-whiskey-glass',
+                label = Config.Models[i].interactionLabel,
+                distance = 1.0,
+                onSelect = function(data)
+                    VendingHandler.currentMachine = data.entity
+                    VendingHandler.currentIndex = i
+                    VendingHandler:Interact()
+                end
+            },
+        }
+        exports.ox_target:addModel(Config.Models[i].model, options)
+    end
+end
+
+function VendingHandler:NearThread()
+    while true do
+        local ped = ESX.PlayerData.ped
+        local coords = GetEntityCoords(ped)
+        local success, machine, index = false, nil, nil
+        for i = 1, #self.storage do
+            local storage = self.storage[i]
+            local Object = GetClosestObjectOfType(coords.x, coords.y, coords.z, storage.distance, joaat(storage.model),
+                false, false, false)
+            if Object and DoesEntityExist(Object) then
+                machine = Object
+                index = i
+                success = true
+            end
+        end
+        self.currentMachine = success and machine or nil
+        self.currentIndex = success and index or nil
+        Wait(500)
+    end
+end
+
+function VendingHandler:HandleNearThread()
+    while true do
+        if self.currentMachine then
+            local interation = self.storage[self.currentIndex]
+            if interation.helpText then
+                if not self.helpText.showing or self.helpText.text ~= interation.helpText then
+                    ESX.TextUI(interation.helpText, "info")
+                    self.helpText.showing = true
+                    self.helpText.text = interation.helpText
+                end
+            end
+        else
+            if self.helpText.showing then
+                ESX.HideUI()
+                self.helpText.showing = false
+                self.helpText.text = ""
+            end
+        end
+        Wait(500)
+    end
+end
+
+function VendingHandler:RegisterInteraction()
+    ESX.RegisterInput("vending_use", "Use Vending Machine", "keyboard", "e", function()
+        print("E Pressed")
+        if IsNuiFocused() then
+            print("NUI Focused")
+            return
+        end
+        if not IsControlEnabled(0, 38) then
+            print("Control Disabled")
+            return
+        end
+        if not self.currentMachine or not self.currentIndex then
+            print("No Machine")
+            return
+        end
+        self:Interact()
+    end)
+end
+
+function VendingHandler:Init()
+    if Config.oxTarget then
+        self:RegisterTargets()
+    else
+        for i = 1, #(Config.Models) do
+            local storage = Config.Models[i]
+            local model = storage.model
+            local distance = 1.0
+            local helpText = storage.interactionLabel
+            self:RegisterMachine(model, distance, helpText)
+        end
+
+        CreateThread(function()
+            self:NearThread()
+        end)
+        CreateThread(function()
+            self:HandleNearThread()
+        end)
+        self:RegisterInteraction()
+    end
+end
+
+CreateThread(function()
+    while not ESX.PlayerLoaded do
+        Wait(500)
+    end
+    VendingHandler:Init()
+end)
+
+function VendingHandler:Interact()
+    local storage = Config.Models[self.currentIndex]
     -- Function Based upon: https://github.com/smallo92/xnVending
     ESX.TriggerServerCallback("vending:canBuyDink", function(canBuy)
         if canBuy then
             local ped = ESX.PlayerData.ped
-            local position = GetOffsetFromEntityInWorldCoords(Object, 0.0, -0.97, 0.05)
-           
-            TaskTurnPedToFaceEntity(ped, Object, -1)
+            local position = GetOffsetFromEntityInWorldCoords(self.currentMachine, 0.0, -0.97, 0.05)
+
+            TaskTurnPedToFaceEntity(ped, self.currentMachine, -1)
             RequestAnimDict(Config.DispenseDict[1])
 
             while not HasAnimDictLoaded(Config.DispenseDict[1]) do
@@ -37,33 +198,35 @@ function Interact(Model, Object)
             HintAmbientAudioBank("VENDING_MACHINE", 0, -1)
 
             SetPedCurrentWeaponVisible(ped, false, true, 1, 0)
-            RequestModel(Config.Models[Model].obj)
-            while not HasModelLoaded(Config.Models[Model].obj) do
+            RequestModel(storage.obj)
+            while not HasModelLoaded(storage.obj) do
                 Wait(0)
             end
             SetPedResetFlag(ped, 322, true)
-            if not IsEntityAtCoord(ped, position, 0.1, 0.1, 0.1, false, true, 0)  then
-                TaskGoStraightToCoord(ped, position, 2.0, 20000, GetEntityHeading(Object), 0.1)
-                while not IsEntityAtCoord(ped, position,0.1, 0.1, 0.1, false, true, 0) do
-                    TaskGoStraightToCoord(ped, position, 5.0, 20000, GetEntityHeading(Object), 0.2)
+            local machineHeading = GetEntityHeading(self.currentMachine)
+            if not IsEntityAtCoord(ped, position, 0.1, 0.1, 0.1, false, true, 0) then
+                TaskGoStraightToCoord(ped, position, 2.0, 20000, machineHeading, 0.1)
+                while not IsEntityAtCoord(ped, position, 0.1, 0.1, 0.1, false, true, 0) do
+                    TaskGoStraightToCoord(ped, position, 5.0, 20000, machineHeading, 0.2)
                     Wait(1000)
                 end
             end
-            TriggerServerEvent("vending:buyDrink", Model)
-            TaskTurnPedToFaceEntity(ped, Object, -1)
+            TriggerServerEvent("vending:buyDrink", self.currentIndex)
+            TaskTurnPedToFaceEntity(ped, self.currentMachine, -1)
             Wait(500)
             TaskPlayAnim(ped, Config.DispenseDict[1], Config.DispenseDict[2], 4.0, 5.0, -1, true, 1, 0, 0, 0)
             Wait(2500)
-            local canModel = CreateObjectNoOffset(Config.Models[Model].obj, position, true, false, false)
+            local canModel = CreateObjectNoOffset(storage.obj, position, true, false, false)
             SetEntityAsMissionEntity(canModel, true, true)
             SetEntityProofs(canModel, false, true, false, false, false, false, 0, false)
-            AttachEntityToEntity(canModel, ped, GetPedBoneIndex(ped, 28422), 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1, 1, 0, 0, 2, 1)
+            AttachEntityToEntity(canModel, ped, GetPedBoneIndex(ped, 28422), 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1, 1, 0, 0, 2,
+                1)
             Wait(3700)
-            TaskPlayAnim(ped, Config.DispenseDict[1], "PLYR_BUY_DRINK_PT2",4.0, 5.0, -1, true, 1, 0, 0, 0)
+            TaskPlayAnim(ped, Config.DispenseDict[1], "PLYR_BUY_DRINK_PT2", 4.0, 5.0, -1, true, 1, 0, 0, 0)
             Wait(1800)
-            TriggerEvent('esx_status:add', 'hunger', Config.Models[Model].hunger)
-            TriggerEvent('esx_status:add', 'thirst', Config.Models[Model].thirst)
-            TaskPlayAnim(ped, Config.DispenseDict[1], "PLYR_BUY_DRINK_PT3",4.0, 5.0, -1, true, 1, 0, 0, 0)
+            TriggerEvent('esx_status:add', 'hunger', storage.hunger)
+            TriggerEvent('esx_status:add', 'thirst', storage.thirst)
+            TaskPlayAnim(ped, Config.DispenseDict[1], "PLYR_BUY_DRINK_PT3", 4.0, 5.0, -1, true, 1, 0, 0, 0)
             Wait(600)
             DetachEntity(canModel, true, true)
             ApplyForceToEntity(canModel, 1, vector3(-6.0, -10.0, -2.0), 0, 0, 0, 0, true, true, false, false, true)
@@ -72,83 +235,9 @@ function Interact(Model, Object)
             ClearPedTasks(ped)
             ReleaseAmbientAudioBank()
             RemoveAnimDict(Config.DispenseDict[1])
-            SetModelAsNoLongerNeeded(Config.Models[Model].obj)
+            SetModelAsNoLongerNeeded(storage.obj)
         else
             ESX.ShowNotification("Cannot Afford Drink!", "error")
         end
-    end, Model)
+    end, self.currentIndex)
 end
-
-local Interactions = {}
-
-local function CreateObjectInteration(name, model, distance, helpText, action)
-    Interactions[#Interactions + 1] = {name = name, model = model, distance = distance, helpText = helpText,action = action}
-end
-
-local Drawing = {showing = false, text = ""}
-
-CreateThread(function()
-    while not ESX.PlayerLoaded do
-        Wait(0)
-    end
-    if Config.oxTarget then
-        for i=1, #(Config.Models) do
-            local options = {
-               {
-                    name = Config.Models[i].model..'-vend',
-                    icon = 'fa-solid fa-whiskey-glass',
-                    label = Config.Models[i].interactionLabel,
-                    distance = 1.0,
-                    onSelect = function(data)
-                        Interact(i, data.entity)
-                    end
-                },
-            }
-            exports.ox_target:addModel(Config.Models[i].model, options)
-        end
-    else
-        for i=1, #(Config.Models) do
-            CreateObjectInteration(Config.Models[i].model.. "-vend", Config.Models[i].model, 1.0, "[E] " ..Config.Models[i].interactionLabel .. " - $"..Config.Models[i].price, function(Object)
-                Interact(i, Object)
-            end)
-        end
-        while true do
-            local Sleep = 1000
-            local PlayerCoords = GetEntityCoords(ESX.PlayerData.ped)
-            local Near = false
-            for _,v in pairs(Interactions) do
-                local interation = v
-                local Object = GetClosestObjectOfType(PlayerCoords, interation.distance, joaat(interation.model), false)
-                if Object and DoesEntityExist(Object) then
-                    local coords = GetEntityCoords(Object)
-                    local dist = #(PlayerCoords - coords)
-                    if dist <= interation.distance then
-                        Near = true
-                        Sleep = 0
-                        if interation.helpText then
-                            if not Drawing.showing or Drawing.text ~= interation.helpText then
-                                ESX.TextUI(interation.helpText, "info")
-                                Drawing.showing = true
-                                Drawing.text = interation.helpText
-                            end
-                        end
-                        if interation.action then
-                            if IsControlJustPressed(0, 38) then
-                                local act = assert(pcall(interation.action, Object))
-                                if not act then
-                                    print("[ERROR] an Error occured during interaction on Object " .. interation.name)
-                                end
-                            end
-                        end
-                    end
-                end
-            end
-            if not Near and Drawing.showing then
-                ESX.HideUI()
-                Drawing.showing = false
-                Drawing.text = ""
-            end
-            Wait(Sleep)
-        end
-    end
-end)
